d5222cf7b7ce9657e4b3233be9a5a4e881169a9e
Pranay Goyal <pranay.goyal@target.com>
2017-02-08 06:06:44 -0600

    Added a Tweet user to the main

---
+++ src/main/java/com/avro/trial/schema/Trial.java
@@ -0,0 +1,12 @@
+package com.avro.trial.schema;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tweet user1 = new Tweet();
+		user1.setTweetId(12);
+		user1.setUser("Alyssa");	
+		}
+
+}
